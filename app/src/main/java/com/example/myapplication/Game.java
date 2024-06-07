c0accad11ce42111fd87a54629a42ebb90ea9669
1999mo <1999mo@naver.com>
2024-06-07 19:34:51 +0900

    feat: added a temp main menu and a button to go the field

--- app/src/main/java/com/example/myapplication/Game.java
+++ app/src/main/java/com/example/myapplication/Game.java
@@ -28,11 +28,13 @@ class Game extends SurfaceView implements SurfaceHolder.Callback{
     private Context context;
     private Knights[] knights;
     private Drawable background;
+    private Drawable mainMenu;
     private float X = 0;
     private float Y = 0;
 
     private boolean toggleSwitch = true;
     private int selectKnight = -1;
+    private boolean stageActivated = false;
 
     public Game(Context context) {
         super(context);
@@ -49,6 +51,7 @@ class Game extends SurfaceView implements SurfaceHolder.Callback{
         knights[1] = new Knights(100, 500, context);
         knights[2] = new Knights(500, 100, context);
         background = context.getResources().getDrawable(R.drawable.background);
+        mainMenu = context.getResources().getDrawable(R.drawable.tempmenu);
 
         setFocusable(true);
     }
@@ -75,20 +78,38 @@ class Game extends SurfaceView implements SurfaceHolder.Callback{
     public void draw(Canvas canvas) {
         super.draw(canvas);
 
-        Rect imageBounds = canvas.getClipBounds();
+        if (stageActivated) {
+            Rect imageBounds = canvas.getClipBounds();
 
-        background.setBounds(imageBounds);
-        background.draw(canvas);
+            background.setBounds(imageBounds);
+            background.draw(canvas);
 
-        //drawUPS(canvas);
-        //drawFPS(canvas);
-        //drawCoordinate(canvas);
-        drawSwitchButton(canvas);
+            //drawUPS(canvas);
+            //drawFPS(canvas);
+            //drawCoordinate(canvas);
+            drawSwitchButton(canvas);
 
-        //draw each object
-        for (Knights knight : knights) {
-            knight.draw(canvas);
+            //draw each object
+            for (Knights knight : knights) {
+                knight.draw(canvas);
+            }
+        } else {
+            Rect imageBounds = canvas.getClipBounds();
+
+            mainMenu.setBounds(imageBounds);
+            mainMenu.draw(canvas);
+            
+            drawMainmenuButton(canvas);
         }
+    }
+
+    private void drawMainmenuButton(Canvas canvas) {
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context, R.color.magenta);
+        paint.setColor(color);
+        paint.setStrokeWidth(10);
+
+        canvas.drawCircle(1500, 500, 100, paint);
     }
 
     private void drawSwitchButton(Canvas canvas) {
@@ -99,7 +120,12 @@ class Game extends SurfaceView implements SurfaceHolder.Callback{
 
         canvas.drawCircle(100, 100, 100, paint);
         paint.setTextSize(50);
-        canvas.drawText("Click : " + toggleSwitch, 100, 300, paint);
+        if(toggleSwitch) {
+            canvas.drawText("Click and point to move", 100, 300, paint);
+        }
+        else {
+            canvas.drawText("Drag and drop to move", 100, 300, paint);
+        }
     }
 
     public void drawUPS(Canvas canvas) {
@@ -132,59 +158,68 @@ class Game extends SurfaceView implements SurfaceHolder.Callback{
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        switch(event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                X = event.getX();
-                Y = event.getY();
-                if (!toggleSwitch) {
-                    for (Knights knight : knights) {
-                        if (knight.isTouched(X, Y)) {
-                            knight.takeActionDown();
+        X = event.getX();
+        Y = event.getY();
+        if(stageActivated) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    if (!toggleSwitch) {
+                        for (Knights knight : knights) {
+                            if (knight.isTouched(X, Y)) {
+                                knight.takeActionDown();
+                            }
+                        }
+                    } else {
+                        if (selectKnight == -1) {
+                            for (int i = 0; i < knights.length; i++) {
+                                if (knights[i].isTouched(X, Y)) {
+                                    knights[i].selectKnight();
+                                    selectKnight = i;
+                                }
+                            }
+                        } else {
+                            knights[selectKnight].unselectKnight(X, Y);
+                            selectKnight = -1;
                         }
                     }
-                }
-                else {
-                    if (selectKnight == -1) {
-                        for (int i = 0; i < knights.length; i++) {
-                            if (knights[i].isTouched(X, Y)) {
-                                knights[i].selectKnight();
-                                selectKnight = i;
+
+                    //Toggle Switch
+                    if (switchClicked()) {
+                        toggleSwitch = !toggleSwitch;
+                    }
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    if (!toggleSwitch) {
+                        for (Knights knight : knights) {
+                            if (knight.getActionDown()) {
+                                knight.setPosition(event.getX(), event.getY());
                             }
                         }
                     }
-                    else {
-                        knights[selectKnight].unselectKnight(X, Y);
-                        selectKnight = -1;
-                    }
-                }
-
-                //Toggle Switch
-                if (switchClicked()) {
-                    toggleSwitch = !toggleSwitch;
-                }
-                break;
-            case MotionEvent.ACTION_MOVE:
-                if (!toggleSwitch) {
-                    for (Knights knight : knights) {
-                        if (knight.getActionDown()) {
-                            knight.setPosition(event.getX(), event.getY());
+                    break;
+                case MotionEvent.ACTION_UP:
+                    if (!toggleSwitch) {
+                        for (Knights knight : knights) {
+                            knight.setActionDown();
                         }
                     }
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-                if (!toggleSwitch) {
-                    for (Knights knight : knights) {
-                        knight.setActionDown();
+                    break;
+            }
+        }
+        else {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    if (getDistance(1500, 500) <= 100) {
+                        stageActivated = true;
                     }
-                }
-                break;
+                    break;
+            }
         }
         return true;
     }
 
     private boolean switchClicked() {
-        double distance = Math.sqrt(Math.pow((X - 100), 2) + Math.pow((Y - 100), 2));
+        double distance = getDistance(100, 100);
         if(distance <= 100) {
             return true;
         }
@@ -199,5 +234,11 @@ class Game extends SurfaceView implements SurfaceHolder.Callback{
 
     public void pause() {
         gameLoop.stopLoop();
+    }
+
+    private double getDistance(float a, float b)
+    {
+        double distance = Math.sqrt(Math.pow((X - a), 2) + Math.pow((Y - b), 2));
+        return distance;
     }
 }
